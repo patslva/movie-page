73fd24406ca10741de9c002720afbe46a73f4022
[Patrick <patsricksilva@gmail.com>
2023-01-11 10:30:55 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
-<script setup></script>
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
 
 <template>
-  <div class="app-container">
-    <Suspense>
-      <RouterView />
-    </Suspense>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-/* *,
-::before,
-::after {
-  padding: 0;
-  margin: 0;
-  color: purple;
-  box-sizing: border-box;
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  background-color: #000000aa;
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 }
-.app-container {
-  width: 100vw;
-  height: 100vh;
-} */
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
